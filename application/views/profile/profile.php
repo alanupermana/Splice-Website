d5da43d73bce69547d43bac2e2f5c0ce4781ed0f
elbiz <l699654@gmail.com>
2019-04-22 22:26:09 +0700

    profile dan styledashboard

--- application/views/profile/profile.php
+++ application/views/profile/profile.php
@@ -1,9 +1,12 @@
-<div class="nav navbar-header" style="margin-bottom:0">
-
+<div class="main">
+<div class="navigasi">
+    <div class="nav navbar-header" style="margin-bottom:0">
     </div>
+</div>
+  <div class="nav1">
     <nav class="navbar navbar-expand-sm  navbar-light">
 
-        <div class="container">
+        <div class="conn container">
           <div class="navbar-brand ml-auto">
             <ul class="navbar-nav">
               <li class="nav-item">
@@ -21,9 +24,10 @@
 
         </div>
     </nav>
-    <div class="content">
-      <div class="col">
-        <div class="card">
+    </div>
+    <div class="konten content">
+      <div class="kolom row">
+        <div class="kartu card">
             <div class="d-flex justify-content-center">
                 <div class="brand_logo_container">
                   <img src="https://belajarbahasakorea.net/wp-content/uploads/2019/03/Gesture-Tangan-Saranghaeyo-4.jpg" class="brand_logo" alt="Logo">
@@ -36,25 +40,25 @@
           <div class="card-text col-7 mx-auto">
               <a href="#" id="editBtn" class="btn btn-primary" style="border-radius:30px;">Edit Profile</a>
           </div>
-          <hr>
-          <div class="card-body col-xs-5">
-            <ul>
-              <li style="list-style-type:none" >
+          <hr class="horizontal">
+          <div class="KartuB card-body col-xs-5">
+            <ul class="ul-kartu">
+              <li class="li-kartu" style="list-style-type:none" >
                <strong>0 Plays</strong>
               </li>
-              <hr>
+              <hr class="horizontal">
 
               <li style="list-style-type:none">
                   <strong>0 Likes</strong>
                 </li>
-                <hr>
+                <hr class="horizontal">
                 <li style="list-style-type:none">
                   <a  href="#">0 Followers</a>
                 </li>
-                <hr>
+                <hr class="horizontal">
               <li style="list-style-type:none">
                 <a href="#">0 Following</a>
-              </li><hr>
+              </li><hr class="horizontal">
               <li class="bio" style="list-style-type:none;border-bottom:2px solid rgb(245, 238, 238)">
                 <strong>Bio</strong>
               </li>
@@ -69,15 +73,42 @@
           &nbsp;
         </div>
 
+          <div class="newCol card col-md-6 ">
+                  
+                  <div class="KartuC card-body col-xs-5" id="body">
+                      <div class="row">
+                          <div class="contents">
+                              <span class="art-wrapper">
+                                  <a href="">
+                                      <span class="art">
+                                          <img src="https://splice-res.cloudinary.com/image/upload/f_auto,q_auto,w_auto/t_cover/app-assets/general/defaultavatarhuge.jpg" width="40px"alt="">
+                                      </span>
+                                  </a>
+                              </span> 
+                          </div>
+                          <div class="card-text col-7">
+                              <span class="project-name">coba</span>
+                              <p class="card-text" id="day">a day ago</p>
+                          </div> 
+                          <div class="card-text col-xs-2 mt-1 ml-auto" >
+                                  <input type="submit" class="btn btn-primary" value="Play" style="border-style:solid">
+                                  <input type="submit" class="btn btn-warning" value="Edit">
+                                  <input type="submit" class="btn btn-danger"  value="Delete">
+                          </div> 
+                      </div> 
+                  </div>
+              </div>
 
 
       </div>
-      <div class="card col-12 ml-auto" style="width:800px" id="nah">
+      <!-- <div class="row col-12 mr-auto" style="width:800px" id="nah">
           <div class="card-body col-12" >
               <div class="col-12 text-center mx-auto" >
                   <p>This user hasn't created any releases yet. <a href="#">Browse explore</a> to find releases from other users.</p>
               </div>
           </div>
-      </div>
+      </div> -->
+  
 
+    </div>
     </div>
