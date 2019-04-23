5e07bfc1074090f67a87fbc6e3e73247883fc0f5
alanupermana <alanupermana@gmail.com>
2019-04-23 21:23:01 +0700

    FINAL

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,7 +4,7 @@
     <p class="textHome-1">Let your creativity flow</p>
     <p class="textHome-2">Join over a million musicians using Splice to find sounds, get gear, and connect with one another.</p>
     <button class="btnHomeSignup-2" type="button" name="SignUp">
-      <a class="aHome" href="<?php echo site_url('Home/SignUp'); ?>">Sign up for free</a>
+      <a class="aHome" href="<?php echo site_url('Splice/SignUp'); ?>">Sign up for free</a>
     </button>
   </div>
 </div>
